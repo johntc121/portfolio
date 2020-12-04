a019e09d804154027f6caa012433d85abcf6332f
John Cripe <39608668+johntc121@users.noreply.github.com>
2020-12-03 20:34:16 -0800

    Fix contact form error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
-import Contact from './components/Contact/Contact'
+// import Contact from './components/Contact/Contact'
 import Home from './components/Home/Home'
 
 import './App.css'
@@ -25,7 +25,7 @@ class App extends Component {
                 <Route exact path='/' component={Home}/>
                 <Route path='/about' component={About}/>
                 <Route path='/projects' component={Projects}/>
-                <Route path='/contact' component={Contact}/>
+                {/* <Route path='/contact' component={Contact}/> */}
               </Switch>
             </div>
           </div>
--- src/components/Contact/EmailForm.js
+++ src/components/Contact/EmailForm.js
@@ -1,157 +1,157 @@
-import React, {Component} from 'react';
-import axios from 'axios';
+// import React, {Component} from 'react';
+// import axios from 'axios';
 
-const validEmailTest = 
-  RegExp(/^(([^<>()\],;:\s@]+([^<>()\],;:\s@]+)*)|(.+))@(([^<>()[,;:\s@]+)+[^<>()[\],;:\s@]{2,})$/i);
+// const validEmailTest = 
+//   RegExp(/^(([^<>()\],;:\s@]+([^<>()\],;:\s@]+)*)|(.+))@(([^<>()[,;:\s@]+)+[^<>()[\],;:\s@]{2,})$/i);
 
-class EmailForm extends Component {
+// class EmailForm extends Component {
 
-    state = {
-        firstName: '',
-        lastName: '',
-        subject: '',
-        email: '',
-        message: '',
-        formErrors: {
-            firstNameError: '',
-            lastNameError: '',
-            emailError: '',
-            messageError: '',
-        },
-        formValid: false
+//     state = {
+//         firstName: '',
+//         lastName: '',
+//         subject: '',
+//         email: '',
+//         message: '',
+//         formErrors: {
+//             firstNameError: '',
+//             lastNameError: '',
+//             emailError: '',
+//             messageError: '',
+//         },
+//         formValid: false
 
-    }
+//     }
 
-    formValid = (formErrors) => {
-        let valid = true;
+//     formValid = (formErrors) => {
+//         let valid = true;
 
-        Object.values(formErrors).forEach(value => {
-            value.length > 0 && (valid = false)
-        });
+//         Object.values(formErrors).forEach(value => {
+//             value.length > 0 && (valid = false)
+//         });
 
-        return valid
-    }
+//         return valid
+//     }
 
-    onChangeHandler = (e) => {
-        const {name, value} = e.target;
-        // console.log("Name: " + name);
-        // console.log("Value: " + value);
+//     onChangeHandler = (e) => {
+//         const {name, value} = e.target;
+//         // console.log("Name: " + name);
+//         // console.log("Value: " + value);
         
-        let formErrors = this.state.formErrors;
+//         let formErrors = this.state.formErrors;
 
-        switch (name) {
-            case 'firstName': {
-                formErrors.firstNameError = value.length < 2 ?
-                    'First Name must be at least 2 characters long' 
-                    : '';
-                break;
-            }
-            case 'lastName': {
-                formErrors.lastNameError = value.length < 2 ?
-                    'Last Name must be at least 2 characters long' 
-                    : '';
-                break;
-            }
-            case 'email': {
-                formErrors.emailError = validEmailTest.test(value) ?
-                    '' 
-                    : 'Please enter a valid email';
-                break;
-            }
-            case 'message': {
-                formErrors.messageError = value.length < 10 ?
-                    'Message must be at least 10 character long' 
-                    : '';
-                break;
-            }
-            default: {
-                break;
-            }
+//         switch (name) {
+//             case 'firstName': {
+//                 formErrors.firstNameError = value.length < 2 ?
+//                     'First Name must be at least 2 characters long' 
+//                     : '';
+//                 break;
+//             }
+//             case 'lastName': {
+//                 formErrors.lastNameError = value.length < 2 ?
+//                     'Last Name must be at least 2 characters long' 
+//                     : '';
+//                 break;
+//             }
+//             case 'email': {
+//                 formErrors.emailError = validEmailTest.test(value) ?
+//                     '' 
+//                     : 'Please enter a valid email';
+//                 break;
+//             }
+//             case 'message': {
+//                 formErrors.messageError = value.length < 10 ?
+//                     'Message must be at least 10 character long' 
+//                     : '';
+//                 break;
+//             }
+//             default: {
+//                 break;
+//             }
             
-        }
+//         }
 
-        this.setState({
-            [name]: value,
-            formErrors,
-        })
-    }
+//         this.setState({
+//             [name]: value,
+//             formErrors,
+//         })
+//     }
 
-    onSubmit = async (e) => {
-        e.preventDefault();
-        console.log(this.state);
+//     onSubmit = async (e) => {
+//         e.preventDefault();
+//         console.log(this.state);
 
-        const {firstName, lastName, email, subject, message} = this.state;
+//         const {firstName, lastName, email, subject, message} = this.state;
 
-        if (this.formValid(this.state.formErrors)) {
-            this.setState({formValid: true})
-            console.log("Submitted")
+//         if (this.formValid(this.state.formErrors)) {
+//             this.setState({formValid: true})
+//             console.log("Submitted")
 
-            const form = await axios.post('/api/form', {
-                firstName,
-                lastName,
-                email,
-                subject,
-                message
-            })
-        }
-        else {
-            this.setState({formValid: false})
-            console.error("Invalid")
-        }
-    }
+//             const form = await axios.post('/api/form', {
+//                 firstName,
+//                 lastName,
+//                 email,
+//                 subject,
+//                 message
+//             })
+//         }
+//         else {
+//             this.setState({formValid: false})
+//             console.error("Invalid")
+//         }
+//     }
     
-    render() {
-        const {formErrors} = this.state;
-        return (
-            <div className="container">
-                <form onSubmit={this.onSubmit} noValidate>
-                    <div className="row">
-                        <div className="input-field col s6">
-                            <input type="text" placeholder="First Name" name="firstName" value={this.state.firstName} onChange={this.onChangeHandler} noValidate/>
-                            {!this.state.formValid && 
-                                <span className='error'>{formErrors.firstNameError}</span>
-                            }
-                            <label htmlFor="firstName">First Name</label>
-                        </div>
-                        <div className="input-field col s6">
-                            <input type="text" placeholder="Last Name" name="lastName" value={this.state.lastName} onChange={this.onChangeHandler} noValidate/>
-                            {!this.state.formValid && 
-                                <span className='error'>{formErrors.lastNameError}</span>
-                            }
-                            <label htmlFor="lastName">Last Name</label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s6">
-                            <input type="text" placeholder="Email" name="email" value={this.state.email} onChange={this.onChangeHandler} noValidate/>
-                            {!this.state.formValid && 
-                                <span className='error'>{formErrors.emailError}</span>
-                            }
-                            <label htmlFor="email">Email</label>
-                        </div>
-                        <div className="input-field col s6">
-                            <input type="text" placeholder="Subject" name="subject" value={this.state.subject} onChange={this.onChangeHandler} noValidate/>
-                            <label htmlFor="subject">Subject</label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <textarea name="message" className="materialize-textarea" placeholder="Message" value={this.state.message} onChange={this.onChangeHandler} noValidate></textarea>
-                            {!this.state.formValid && 
-                                <span className='error'>{formErrors.messageError}</span>
-                            }
-                            <label htmlFor="message">Message</label>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <button className="btn waves-effect waves-light" type="submit" name="action">Send
-                            <i className="material-icons right">send</i>
-                        </button>
-                    </div>
-                </form>
-            </div>
-        )
-    }
-}
+//     render() {
+//         const {formErrors} = this.state;
+//         return (
+//             <div className="container">
+//                 <form onSubmit={this.onSubmit} noValidate>
+//                     <div className="row">
+//                         <div className="input-field col s6">
+//                             <input type="text" placeholder="First Name" name="firstName" value={this.state.firstName} onChange={this.onChangeHandler} noValidate/>
+//                             {!this.state.formValid && 
+//                                 <span className='error'>{formErrors.firstNameError}</span>
+//                             }
+//                             <label htmlFor="firstName">First Name</label>
+//                         </div>
+//                         <div className="input-field col s6">
+//                             <input type="text" placeholder="Last Name" name="lastName" value={this.state.lastName} onChange={this.onChangeHandler} noValidate/>
+//                             {!this.state.formValid && 
+//                                 <span className='error'>{formErrors.lastNameError}</span>
+//                             }
+//                             <label htmlFor="lastName">Last Name</label>
+//                         </div>
+//                     </div>
+//                     <div className="row">
+//                         <div className="input-field col s6">
+//                             <input type="text" placeholder="Email" name="email" value={this.state.email} onChange={this.onChangeHandler} noValidate/>
+//                             {!this.state.formValid && 
+//                                 <span className='error'>{formErrors.emailError}</span>
+//                             }
+//                             <label htmlFor="email">Email</label>
+//                         </div>
+//                         <div className="input-field col s6">
+//                             <input type="text" placeholder="Subject" name="subject" value={this.state.subject} onChange={this.onChangeHandler} noValidate/>
+//                             <label htmlFor="subject">Subject</label>
+//                         </div>
+//                     </div>
+//                     <div className="row">
+//                         <div className="input-field col s12">
+//                             <textarea name="message" className="materialize-textarea" placeholder="Message" value={this.state.message} onChange={this.onChangeHandler} noValidate></textarea>
+//                             {!this.state.formValid && 
+//                                 <span className='error'>{formErrors.messageError}</span>
+//                             }
+//                             <label htmlFor="message">Message</label>
+//                         </div>
+//                     </div>
+//                     <div className="row">
+//                         <button className="btn waves-effect waves-light" type="submit" name="action">Send
+//                             <i className="material-icons right">send</i>
+//                         </button>
+//                     </div>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
 
-export default EmailForm;
+// export default EmailForm;
